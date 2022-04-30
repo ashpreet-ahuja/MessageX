874dae95f87c38e1a300e7a62e957e3341f02b39
Ashpreet <ashpreetahuja3sprime@gmail.com>
2022-04-30 23:03:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/messagex/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.messagex.listeners;
+
+import com.example.messagex.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/messagex/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.messagex.listeners;
+
+import com.example.messagex.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
